b02614d6e051f8918193d9486dcf44bee13b93a4
Fernanda Nolasco <104088221+paolafdanol@users.noreply.github.com>
2022-04-20 12:49:23 -0500

    Add files via upload

---
+++ main7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	char nombre [20];
+	printf("¿Cual es tu nombre?\n");
+	scanf("%s",&nombre);
+	printf("Tu nombre es:%s\n",nombre);
+	return 0;
+}
